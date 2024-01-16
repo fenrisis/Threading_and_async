3b8d2e2d93c53ee1ca13271995152c6353f6cd0f
Leonid3# <116304748+fenrisis@users.noreply.github.com>
2024-01-16 20:14:58 +0100

    Update thread_and_lock.py
    added print for evry thread

--- thread_and_lock.py
+++ thread_and_lock.py
@@ -7,17 +7,18 @@ counter = 0
 mutex = threading.Lock()
 
 
-def increment_counter():
+def increment_counter(thread_id):
     global counter
-    for i in range(100):
+    for _ in range(100):
         with mutex:
             counter += 1
+            print(f"Thread {thread_id} incremented counter to {counter}")
 
 
 # 3 threads
 threads = []
 for i in range(3):
-    thread = threading.Thread(target=increment_counter())
+    thread = threading.Thread(target=increment_counter, args=(i,))
     thread.start()
     threads.append(thread)
 
